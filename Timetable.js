f0b0c1fc82f607b0f9c6357265009412a71205f5
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2022-04-07 01:57:55 +0900

    Add files via upload

---
+++ Timetable.js
@@ -0,0 +1,8 @@
+function handleOnChange(e) {
+    // 선택된 데이터 가져오기
+    const data = e.value;
+
+    // 데이터 출력
+    document.getElementById('result').innerText = data;
+    document.getElementById("TimeTable").src="시간표들/대덕고 "+data+" 자세히.png";
+}
