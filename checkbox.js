754969e4f2559628ddde6881f1039ac0e69aaae3
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2022-04-13 00:04:14 +0900

    Add files via upload

--- checkbox.js
+++ checkbox.js
@@ -3,12 +3,23 @@ function is_checked() {
     // document.getElementById("p123").innerText = C;
     if (C == true) {
         document.getElementById("ChangeTimeTable").style.visibility = "hidden";
+
         document.getElementById("TimeTable").src = "시간표들/모의고사.png";
-        document.getElementById("Bab_And_Dday").style.visibility = "hidden";
+
+        document.getElementById("Bab_And_Dday").style.display = "none";
+
+        document.getElementById("Pre_Test").style.visibility = "visible";
+        document.getElementById("Pre_Test").style.display = "block";
+
     } else {
         document.getElementById("ChangeTimeTable").style.visibility = "visible";
+
         document.getElementById("TimeTable").src = "시간표들/대덕고 3-1 자세히.png";
         document.getElementById('result').innerText = "1반";
-        document.getElementById("Bab_And_Dday").style.visibility = "visible";
+
+        document.getElementById("Bab_And_Dday").style.display = "block";
+
+        document.getElementById("Pre_Test").style.visibility = "hidden";
+        document.getElementById("Pre_Test").style.display = "none";
     }
 }
