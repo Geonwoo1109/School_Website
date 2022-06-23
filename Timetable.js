478711f2f7c90608644ea42ee6b3270577b14586
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2022-06-23 23:36:23 +0900

    Add files via upload

--- Timetable.js
+++ Timetable.js
@@ -1,8 +1,8 @@
 function handleOnChange(e) {
     // 선택된 데이터 가져오기
-    const data = e.value;
+    const timetable = e.value;
 
     // 데이터 출력
-    document.getElementById('result_Timetable').innerText = data;
-    document.getElementById("TimeTable").src="시간표들(배경 제거)/대덕고 "+data+" 자세히.png";
+    document.getElementById('result_Timetable').innerText = timetable;
+    document.getElementById("TimeTable").src="시간표들(배경 제거)/대덕고 "+timetable+" 자세히.png";
 }
